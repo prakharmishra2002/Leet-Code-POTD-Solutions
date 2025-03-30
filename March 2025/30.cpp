95bf20a33d976b8a7ee49e81b35e636ad34bfb96
Prakhar Mishra <103823400+prakharmishra2002@users.noreply.github.com>
2025-03-30 23:15:33 +0530

    Update 30.cpp

--- March 2025/30.cpp
+++ March 2025/30.cpp
@@ -1,70 +1,22 @@
-/*You are required to complete this method*/
 class Solution {
-  public:
-    int startStation(vector<int>& gas, vector<int>& cost) {
-        int total = 0, curr = 0, start = 0;
-        for (int i = 0; i < gas.size(); i++) {
-            int diff = gas[i] - cost[i];
-            total += diff;
-            curr += diff;
-            if (curr < 0) start = i + 1, curr = 0;
+public:
+    vector<int> partitionLabels(string s) {
+        // Stores the last index of each character in 's'
+        vector<int> lastOccurrence(26, 0);
+        for (int i = 0; i < s.size(); ++i) {
+            lastOccurrence[s[i] - 'a'] = i;
         }
-        return total >= 0 ? start : -1;
-    }
-};
 
-2)
-class Solution {
-  public:
-    int startStation(vector<int>& gas, vector<int>& cost) {
-        int total = 0, curr = 0, start = 0;
-        for (int i = 0; i < gas.size(); i++) {
-            total += gas[i] - cost[i];
-            curr += gas[i] - cost[i];
-            if (curr < 0) {
-                start = i + 1;
-                curr = 0;
+        int partitionEnd = 0, partitionStart = 0;
+        vector<int> partitionSizes;
+        for (int i = 0; i < s.size(); ++i) {
+            partitionEnd = max(partitionEnd, lastOccurrence[s[i] - 'a']);
+            // End of the current partition
+            if (i == partitionEnd) {
+                partitionSizes.push_back(i - partitionStart + 1);
+                partitionStart = i + 1;
             }
         }
-        return total >= 0 ? start : -1;
-    }
-};
-
-3)
-class Solution {
-  public:
-    int startStation(vector<int>& gas, vector<int>& cost) {
-        int total = 0, sum = 0, start = 0;
-        for (int i = 0; i < gas.size(); i++) {
-            int diff = gas[i] - cost[i];
-            total += diff;
-            sum += diff;
-            if (sum < 0) {
-                start = i + 1;
-                sum = 0;
-            }
-        }
-        return total >= 0 ? start : -1;
-    }
-};
-
-4)
-class Solution {
-  public:
-    int startStation(vector<int>& gas, vector<int>& cost) {
-        int total = 0, curr = 0, start = 0;
-        int n = gas.size();
-        for (int i = 0; i < 2 * n; i++) {
-            int index = i % n;
-            int diff = gas[index] - cost[index];
-            total += diff;
-            curr += diff;
-            if (curr < 0) {
-                start = index + 1;
-                curr = 0;
-                if (start >= n) break;
-            }
-        }
-        return total >= 0 ? start : -1;
+        return partitionSizes;
     }
 };
