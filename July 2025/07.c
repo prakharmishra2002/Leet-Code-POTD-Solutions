c3619eb09879c01a0500e4ef77fe8381f4030917
Prakhar Mishra <103823400+prakharmishra2002@users.noreply.github.com>
2025-07-07 23:06:30 +0530

    Create 07.c

---
+++ July 2025/07.c
@@ -0,0 +1,136 @@
+#define MIN_QUEUE_SIZE 64
+
+typedef struct Element {
+    int data;
+} Element;
+
+typedef bool (*compare)(const void *, const void *);
+
+typedef struct PriorityQueue {
+    Element *arr;
+    int capacity;
+    int queueSize;
+    compare lessFunc;
+} PriorityQueue;
+
+static bool greater(const void *a, const void *b) {
+    Element *e1 = (Element *)a;
+    Element *e2 = (Element *)b;
+    return e1->data > e2->data;
+}
+
+static void memswap(void *m1, void *m2, size_t size) {
+    unsigned char *a = (unsigned char *)m1;
+    unsigned char *b = (unsigned char *)m2;
+    while (size--) {
+        *b ^= *a ^= *b ^= *a;
+        a++;
+        b++;
+    }
+}
+
+static void swap(Element *arr, int i, int j) {
+    memswap(&arr[i], &arr[j], sizeof(Element));
+}
+
+static void down(Element *arr, int size, int i, compare cmpFunc) {
+    for (int k = 2 * i + 1; k < size; k = 2 * k + 1) {
+        if (k + 1 < size && cmpFunc(&arr[k], &arr[k + 1])) {
+            k++;
+        }
+        if (cmpFunc(&arr[k], &arr[(k - 1) / 2])) {
+            break;
+        }
+        swap(arr, k, (k - 1) / 2);
+    }
+}
+
+PriorityQueue *createPriorityQueue(compare cmpFunc) {
+    PriorityQueue *obj = (PriorityQueue *)malloc(sizeof(PriorityQueue));
+    obj->capacity = MIN_QUEUE_SIZE;
+    obj->arr = (Element *)malloc(sizeof(Element) * obj->capacity);
+    obj->queueSize = 0;
+    obj->lessFunc = cmpFunc;
+    return obj;
+}
+
+void heapfiy(PriorityQueue *obj) {
+    for (int i = obj->queueSize / 2 - 1; i >= 0; i--) {
+        down(obj->arr, obj->queueSize, i, obj->lessFunc);
+    }
+}
+
+void enQueue(PriorityQueue *obj, Element *e) {
+    // we need to alloc more space, just twice space size
+    if (obj->queueSize == obj->capacity) {
+        obj->capacity *= 2;
+        obj->arr = realloc(obj->arr, sizeof(Element) * obj->capacity);
+    }
+    memcpy(&obj->arr[obj->queueSize], e, sizeof(Element));
+    for (int i = obj->queueSize;
+         i > 0 && obj->lessFunc(&obj->arr[(i - 1) / 2], &obj->arr[i]);
+         i = (i - 1) / 2) {
+        swap(obj->arr, i, (i - 1) / 2);
+    }
+    obj->queueSize++;
+}
+
+Element *deQueue(PriorityQueue *obj) {
+    swap(obj->arr, 0, obj->queueSize - 1);
+    down(obj->arr, obj->queueSize - 1, 0, obj->lessFunc);
+    Element *e = &obj->arr[obj->queueSize - 1];
+    obj->queueSize--;
+    return e;
+}
+
+bool isEmpty(const PriorityQueue *obj) { return obj->queueSize == 0; }
+
+Element *front(const PriorityQueue *obj) {
+    if (obj->queueSize == 0) {
+        return NULL;
+    } else {
+        return &obj->arr[0];
+    }
+}
+
+void clear(PriorityQueue *obj) { obj->queueSize = 0; }
+
+int size(const PriorityQueue *obj) { return obj->queueSize; }
+
+void freeQueue(PriorityQueue *obj) {
+    free(obj->arr);
+    free(obj);
+}
+
+int cmp(const void *a, const void *b) {
+    return (*(int **)a)[0] - (*(int **)b)[0];
+};
+
+int maxEvents(int **events, int eventsSize, int *eventsColSize) {
+    int n = eventsSize;
+    int maxDay = 0;
+    for (int i = 0; i < eventsSize; i++) {
+        maxDay = fmax(maxDay, events[i][1]);
+    }
+
+    PriorityQueue *pq = createPriorityQueue(greater);
+    qsort(events, eventsSize, sizeof(events[0]), cmp);
+    int ans = 0;
+    for (int i = 1, j = 0; i <= maxDay; i++) {
+        while (j < n && events[j][0] <= i) {
+            Element e = {events[j][1]};
+            enQueue(pq, &e);
+            j++;
+        }
+        while (!isEmpty(pq) && front(pq)->data < i) {
+            deQueue(pq);
+        }
+        if (!isEmpty(pq)) {
+            deQueue(pq);
+            ans++;
+        }
+    }
+
+    freeQueue(pq);
+    return ans;
+}
